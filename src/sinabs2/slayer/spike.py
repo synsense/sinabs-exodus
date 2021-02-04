9e088602f9ce4e2698571aeac0a044dedb8b0645
Felix Bauer <felix.bauer@aictx.ai>
2021-02-04 13:11:58 +0100

    Fix vmem in backward

--- src/sinabs2/slayer/spike.py
+++ src/sinabs2/slayer/spike.py
@@ -3,12 +3,13 @@ import sinabsslayerCuda
 
 
 class SpikeFunction(torch.autograd.Function):
-
     @staticmethod
     def forward(ctx, membranePotential, refractoryResponse, threshold, tauRho):
         threshold = threshold
 
-        spikes, vmem_before_spikes = sinabsslayerCuda.getSpikes(membranePotential.contiguous(), refractoryResponse, threshold, 1.0)
+        spikes, vmem_before_spikes = sinabsslayerCuda.getSpikes(
+            membranePotential.contiguous(), refractoryResponse, threshold, 1.0
+        )
         pdfTimeConstant = tauRho * threshold
         ctx.threshold = threshold
         ctx.pdfTimeConstant = pdfTimeConstant
@@ -19,12 +20,17 @@ class SpikeFunction(torch.autograd.Function):
     @staticmethod
     def backward(ctx, gradOutput):
         membranePotential, = ctx.saved_tensors
-        vmem = (membranePotential - ctx.threshold / 2) % ctx.threshold
-        vmem_below = membranePotential * (membranePotential < ctx.threshold)
-        vmem_above = vmem * (membranePotential >= ctx.threshold)
+        vmem_shifted = membranePotential - ctx.threshold / 2
+        vmem_periodic = vmem_shifted % ctx.threshold
+        vmem_below = vmem_shifted * (membranePotential < ctx.threshold)
+        vmem_above = vmem_periodic * (membranePotential >= ctx.threshold)
         vmem_new = vmem_above + vmem_below
-        spikePdf = 1 / ctx.pdfTimeConstant * torch.exp(-torch.abs(vmem_new - ctx.threshold / 2) / ctx.pdfTimeConstant)
-        #spikePdf = (membranePotential >= (ctx.threshold - 0.5)).float()
+        spikePdf = (
+            1
+            / ctx.pdfTimeConstant
+            * torch.exp(-torch.abs(vmem_new - ctx.threshold / 2) / ctx.pdfTimeConstant)
+        )
+        # spikePdf = (membranePotential >= (ctx.threshold - 0.5)).float()
 
         return gradOutput * spikePdf, None, None, None, None
 
