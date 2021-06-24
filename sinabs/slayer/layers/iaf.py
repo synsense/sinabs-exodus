28130ca5bf9332a665868e8178c14c3050ae94b0
Felix Bauer <felix.bauer@aictx.ai>
2021-06-24 17:32:08 +0200

    IAF layer compatible with new sinabs convention

--- sinabs/slayer/layers/iaf.py
+++ sinabs/slayer/layers/iaf.py
@@ -1,16 +1,19 @@
 from typing import Optional
 
-from torch import nn
-
 from sinabs.slayer.kernels import heaviside_kernel
 from sinabs.slayer.psp import generateEpsp
 from sinabs.slayer.spike import spikeFunction
+from sinabs.layers.pack_dims import squeeze_class
+from sinabs.layers import IAF
 
 
-class SpikingLayer(nn.Module):
+__all__ = ["IAFSlayer", "IAFSlayerSqueeze"]
+
+
+class IAFSlayer(IAF):
     def __init__(
         self,
-        t_sim: int,
+        num_timesteps: int,
         threshold: float = 1.0,
         membrane_subtract: Optional[float] = None,
         tau_learning: float = 0.5,
@@ -22,7 +25,7 @@ class SpikingLayer(nn.Module):
 
         Parameters:
         -----------
-        t_sim: int
+        num_timesteps: int
             Number of timesteps per sample.
         threshold: float
             Spiking threshold of the neuron.
@@ -42,16 +45,16 @@ class SpikingLayer(nn.Module):
         self.threshold = threshold
         self.scale_grads = scale_grads
         self.tau_learning = tau_learning
-        self.t_sim = t_sim
+        self.num_timesteps = num_timesteps
 
         if threshold_low is not None:
             raise NotImplementedError("Lower threshold not implemented for this layer.")
 
-        epsp_kernel = heaviside_kernel(size=t_sim, scale=1.0)
+        epsp_kernel = heaviside_kernel(size=num_timesteps, scale=1.0)
 
         if membrane_subtract is None:
             membrane_subtract = threshold
-        ref_kernel = heaviside_kernel(size=t_sim, scale=membrane_subtract)
+        ref_kernel = heaviside_kernel(size=num_timesteps, scale=membrane_subtract)
         assert ref_kernel.ndim == 1
 
         # Blank parameter place holders
@@ -68,9 +71,7 @@ class SpikingLayer(nn.Module):
         ----------
         spike_input: torch.tensor
             Spike input raster. May take non-binary integer values.
-            Expected shapes:
-                5D: (n_batches, t_sim, channels, height, width)
-                or 4D: (n_batches x t_sim, channels, height, width)
+            Expected shape: (n_batches, num_timesteps, ...)
 
         Returns
         -------
@@ -78,30 +79,12 @@ class SpikingLayer(nn.Module):
             Output spikes. Same shape as `spike_input`.
         """
 
-        if spike_input.ndim == 5:
-            # Expected input dimension: (n_batches, t_sim, *neuron_shape )
-            seperate_batch_time = True
-
-        else:
-            # Expected input dimension: (n_batches x t_sim, *neuron_shape )
-            seperate_batch_time = False
-            neuron_shape = spike_input.shape[1:]
-            try:
-                # Separate batch and time dimensions
-                spike_input = spike_input.reshape(-1, self.t_sim, *neuron_shape)
-            except RuntimeError:
-                raise ValueError(
-                    f"First input dimension (time) must be multiple of {self.t_sim}"
-                    + f" but is {spike_input.shape[0]}."
-                )
-        # Shape is now (n_batches, t_sim, *neuron_shape)
-
-        # Move time to last dimension
-        spike_input = spike_input.movedim(1, -1)  # -> (n_batches, *neuron_shape, t_sim)
+        # Move time to last dimension -> (n_batches, *neuron_shape, num_timesteps)
+        spike_input = spike_input.movedim(1, -1)
         # Flatten out all dimensions that can be processed in parallel and ensure contiguity
         shape_before_flat = spike_input.shape
         spike_input = spike_input.reshape(-1, spike_input.shape[-1]).contiguous()
-        # -> (n_parallel, t_sim)
+        # -> (n_parallel, num_timesteps)
         assert spike_input.ndim == 2
         assert spike_input.is_contiguous()
 
@@ -117,18 +100,17 @@ class SpikingLayer(nn.Module):
 
         assert vmem.ndim == 2
 
-        # Restore original 5d-shape: (n_batches, t_sim, *neuron_shape)
+        # Restore original shape: (n_batches, num_timesteps, *neuron_shape)
         vmem = vmem.reshape(*shape_before_flat).movedim(-1, 1)
         output_spikes = output_spikes.reshape(*shape_before_flat).movedim(-1, 1)
 
-        if not seperate_batch_time:
-            # Combine batch and time dimensions -> (n_batches x t_sim, *neuron_shape)
-            vmem = vmem.reshape(-1, *vmem.shape[2:])
-            output_spikes = output_spikes.reshape(-1, *output_spikes.shape[2:])
-
         self.vmem = vmem.clone()
-        self.tw = self.t_sim
+        self.tw = self.num_timesteps
 
         self.spikes_number = output_spikes.sum()
 
         return output_spikes
+
+
+# Class to accept data with batch and time dimensions combined
+IAFSlayerSqueeze = squeeze_class(IAFSlayer)
